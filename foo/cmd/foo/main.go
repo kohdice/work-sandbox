c883276da3bf5d961268e3b28cbf6b1938802714
kohdice <kohdice@gmail.com>
2025-03-27 10:25:04 +0900

    feat: Add greet to hoge

--- foo/cmd/foo/main.go
+++ foo/cmd/foo/main.go
@@ -1,7 +1,11 @@
 package main
 
-import "github.com/kohdice/work-sandbox/baz/pkg/greet"
+import (
+	"github.com/kohdice/work-sandbox/baz/pkg/greet"
+	hoge "github.com/kohdice/work-sandbox/hoge/pkg/greet"
+)
 
 func main() {
 	greet.Greet("foo")
+	hoge.Greet("foo")
 }
